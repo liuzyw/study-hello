1af8eacb4ffa71f063c0636eb02762413a3bd31e
liuzhaoyuan <liuzhaoyuan@lzyMacPro.local>
2017-10-03 09:44:01 +0800

    dir

--- spring-web/src/test/java/BaseTest.java
+++ spring-web/src/test/java/BaseTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-    "classpath:applicationContext.xml"})
+    "classpath:spring/applicationContext.xml"})
 public class BaseTest {
 
 }
--- spring-web/src/test/java/com/study/spring/controller/RedisControllerTest.java
+++ spring-web/src/test/java/com/study/spring/controller/RedisControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-    "classpath:applicationContext.xml"})
+    "classpath:spring/applicationContext.xml"})
 public class RedisControllerTest {
 
     @Autowired
