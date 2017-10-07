c14126910368c2ed45aabf0fd98b4e7694419e39
liuzhaoyuan <liuzhaoyuan@lzyMacPro.local>
2017-10-07 20:48:19 +0800

    spring boot web mybatis

--- spring-boot-web/src/main/java/com/study/spring/boot/web/controller/SpringBootWebTestController.java
+++ spring-boot-web/src/main/java/com/study/spring/boot/web/controller/SpringBootWebTestController.java
@@ -1,8 +1,11 @@
 package com.study.spring.boot.web.controller;
 
+import com.study.spring.boot.web.mapper.BookDao;
+import com.study.spring.boot.web.po.Book;
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +21,19 @@ public class SpringBootWebTestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootWebTestController.class);
 
+    @Autowired
+    private BookDao bookDao;
+
     @RequestMapping("/hello")
     public String home() {
         LOGGER.info(" spring boot web log ...");
         return "... Hello My First Spring Boot Web Test ..." + new Date();
+    }
+
+    @RequestMapping("/getBookById")
+    public Book getBookById(){
+        Book book = bookDao.getBookById(11);
+        return book;
     }
 
 
---
+++ spring-boot-web/src/main/java/com/study/spring/boot/web/po/Book.java
@@ -0,0 +1,60 @@
+package com.study.spring.boot.web.po;
+
+
+/**
+ * Created on 2017-09-30
+ *
+ * @author liuzhaoyuan
+ */
+
+public class Book {
+
+    private Integer id;
+    private String name;
+    private String type;
+    private Integer price;
+
+    public Book() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+            "name='" + name + '\'' +
+            ", type='" + type + '\'' +
+            ", price=" + price +
+            '}';
+    }
+}
