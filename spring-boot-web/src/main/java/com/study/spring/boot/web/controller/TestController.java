1321857fb4ac49972346a11e5bb057093fdc13ee
liuzhaoyuan <liuzhaoyuan@lzyMacPro.local>
2017-10-14 10:36:35 +0800

    log4j2

--- spring-boot-web/src/main/java/com/study/spring/boot/web/controller/TestController.java
+++ spring-boot-web/src/main/java/com/study/spring/boot/web/controller/TestController.java
@@ -1,13 +1,8 @@
 package com.study.spring.boot.web.controller;
 
-import com.study.spring.boot.web.mapper.BookDao;
-import com.study.spring.boot.web.po.Book;
 import com.study.util.date.DateUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
