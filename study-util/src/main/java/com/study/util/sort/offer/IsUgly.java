9e8ede2a94ed2dd89886130da246a03a57ee7ea1
liuzhaoyuan <liuzhaoyuan@lzyMacPro.local>
2018-11-20 22:58:54 +0800

    youhua

--- study-util/src/main/java/com/study/util/sort/offer/IsUgly.java
+++ study-util/src/main/java/com/study/util/sort/offer/IsUgly.java
@@ -8,7 +8,9 @@ package com.study.util.sort.offer;
 public class IsUgly {
 
     public static void main(String[] args) {
-        System.out.println(getUglyNumber2(1500));
+        System.out.println(getUglyNumber2(1501));
+
+        System.out.println(aaa(1500));
     }
 
     public static int getUglyNumber2(int index) {
@@ -48,6 +50,42 @@ public class IsUgly {
     private static int min(int n1, int n2, int n3) {
         int min = n1 < n2 ? n1 : n2;
         return min < n3 ? min : n3;
+    }
+
+    private static int aaa(int n) {
+
+        int[] arr = new int[n + 1];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6;
+
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+
+        for (int i = 6; i <= n; i++) {
+
+            int cur = arr[i - 1];
+
+            while (arr[index2] * 2 <= cur) {
+                index2++;
+            }
+            while (arr[index3] * 3 <= cur) {
+                index3++;
+            }
+            while (arr[index5] * 5 <= cur) {
+                index5++;
+            }
+            int next = min(arr[index2] * 2, arr[index3] * 3, arr[index5] * 5);
+            arr[i] = next;
+
+        }
+
+        return arr[n];
+
     }
 
 }
